93e72a16dbcbe9f145ffabb82ccb43cec90bd777
Daniell Algar <daniell.algar@varvet.com>
2016-12-12 20:16:00 +0100

    Done day6.

---
+++ day6.m
@@ -0,0 +1,22 @@
+function [errorCorr, origMsg] = day6
+    input = readInput('day6_input.txt');
+    errorCorr = '';
+    origMsg = '';
+
+    for i = 1 : length(input{1, 1})
+        column = cellfun(@(x) x(i), input);
+        errorCorr = [errorCorr, mode(column)];
+        
+        [uniqueA, ~, J] = unique(column);
+        occ = histc(J, 1 : numel(uniqueA));
+        [~, l] = min(occ);
+        origMsg = [origMsg, uniqueA(l)];
+    end
+end
+
+function input = readInput(fileName)
+    fId = fopen(fileName);
+    input = textscan(fId, '%s', 'delimiter', '\n');
+    fclose(fId);
+    input = input{1, 1};
+end
