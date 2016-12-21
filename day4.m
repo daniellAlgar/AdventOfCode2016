6134f16961ececdc5935091462eaba581328d41f
Daniell Algar <daniell.algar@varvet.com>
2016-12-21 22:09:15 +0100

    Done day 4.

--- day4.m
+++ day4.m
@@ -1,4 +1,4 @@
-function sectorIdSum = day4
+function [sectorIdSum, words, val] = day4
     input = readInput('day4_input.txt');
     val = cell(0, 3);
 
@@ -10,11 +10,32 @@ function sectorIdSum = day4
         if strcmp(uniq(ind(1 : 5)), input{i, 3})
             val{end + 1, 1} = input{i, 1};
             val{end, 2} = input{i, 2};
+            val{end, 3} = input{i, 3};
         end
     end
     
-    a = cellfun(@(x) str2double(x), val(:, 2), 'UniformOutput', false);
-    sectorIdSum = sum([a{:}]);
+    sectors = cellfun(@(x) str2double(x), val(:, 2), 'UniformOutput', false);
+    sectorIdSum = sum([sectors{:}]);
+    
+    words = cell(0, 1);
+    for i = 1 : size(val, 1)
+        words{end + 1, 1} = shift(val{i, 1}, val{i, 2});
+    end
+end
+
+function word = shift(string, sectorId)
+    alph = 'abcdefghijklmnopqrstuvwxyz';
+    word = '';
+    
+    for i = 1 : length(string)
+        ind = find(alph == string(i));
+        r = rem(str2double(sectorId) + ind, 26);
+        if r == 0
+            word = [word, alph(ind)];
+        else
+            word = [word, alph(r)];
+        end
+    end
 end
 
 function res = readInput(fileName)
